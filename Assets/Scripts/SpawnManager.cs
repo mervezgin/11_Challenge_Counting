d5a7851f85c2e7aa794be0063f5e08251a5b22f1
mervezgin <mervezgin@gmail.com>
2024-04-26 14:55:06 +0300

    created spawn manager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField] GameObject[] ballPrefabs;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnBalls", 1, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnBalls()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-6, 6), 6, Random.Range(-6, 6));
+        int index = Random.Range(0, ballPrefabs.Length);
+        Instantiate(ballPrefabs[index], spawnPos, ballPrefabs[index].transform.rotation);
     }
 }
